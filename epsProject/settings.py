422ef78a517bd1c9f5260f0f63e27b1fd7312b09
Oliver <oliverdereverde@hotmail.com>
2022-01-03 16:33:28 -0500

    Correction

--- epsProject/settings.py
+++ epsProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v3=#*axitya!hz43tb2-qaqiwf%t_1-&ko#o3irc^pk=w8k)n0'
+SECRET_KEY = 'XXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
